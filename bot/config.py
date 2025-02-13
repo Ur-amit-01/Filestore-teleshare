2a69530554bd51f293e2e9838c1f0d450b55e90c
Amit <heroku467@gmail.com>
2025-02-13 16:17:43 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -42,7 +42,7 @@ class Config(BaseSettings):
 
     API_ID: int = "22012880"
     API_HASH: str = "5b0e07f5a96d48b704eb9850d274fe1d"
-    BOT_TOKEN: str
+    BOT_TOKEN: str = "7879292880:AAH1xJPBet9uBwhnI8o-AZzynRpzIjVwatY"
     BOT_WORKER: int = 8
     BOT_SESSION: str = "Zaws-File-Share"
     BOT_MAX_MESSAGE_CACHE_SIZE: int = 100
@@ -52,11 +52,11 @@ class Config(BaseSettings):
 
     # Bot main config
     RATE_LIMITER: bool = True
-    BACKUP_CHANNEL: int = "-1002293450334"
-    ROOT_ADMINS_ID: list[int] = [7728066109,2031106491]
+    BACKUP_CHANNEL: int = "-1002439464108"
+    ROOT_ADMINS_ID: list[int] = [6803505727,827932553]
     PRIVATE_REQUEST: bool = True
     PROTECT_CONTENT: bool = False
-    FORCE_SUB_CHANNELS: list[int] = [-1002242369773]
+    FORCE_SUB_CHANNELS: list[int] = [-1002468887113]
     AUTO_GENERATE_LINK: bool = True
 
     # Injected Config
